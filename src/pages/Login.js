8bbc4550800d9d3565f4b422817c4b88d1d1f8fb
Gabriel-Lillo <gabriel.lillovjm@gmail.com>
2021-07-14 23:49:53 +0000

    signup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,17 +11,17 @@ const Login = () => {
 
                     <div className="login-form">
                         <div className="control-group">
-                            <input type="text" className="login-field" value="" placeholder="username" id="login-name" />
+                            <input type="text" className="login-field" value="" placeholder="Usuario" id="login-name" />
                             <label className="login-field-icon fui-user" for="login-name"></label>
                         </div>
 
                         <div className="control-group">
-                            <input type="password" className="login-field" value="" placeholder="password" id="login-pass" />
+                            <input type="password" className="login-field" value="" placeholder="Contraseña" id="login-pass" />
                             <label className="login-field-icon fui-lock" for="login-pass"></label>
                         </div>
 
-                        <a className="btn btn-primary btn-large btn-block" href="">login</a>
-                        <a className="login-link" href="">Sign Up</a>
+                        <a className="btn btn-primary btn-large btn-block" href="">Login</a>
+                        <a className="login-link" href="/signup">Sign Up</a>
                     </div>
                 </div>
             </div>
