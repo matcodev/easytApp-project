05d494412f6ffbbe199fae751863c035c9f95711
Gabriel-Lillo <gabriel.lillovjm@gmail.com>
2021-07-22 23:40:32 +0000

    archivo error agregado

--- src/pages/Sales.js
+++ src/pages/Sales.js
@@ -5,15 +5,33 @@ import SalesChart from '../components/sales/components/SalesChart';
 const Sales = () => {
 
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1 className="pt-4">Ingresos</h1>
-      <div className="charts-sales">
-        <div className="card">
-          <SalesChart />
-        </div>
-        <div className="card">
-          {/* <PieChartSales /> */}
-        </div>
+    // <div style={{ textAlign: "center" }}>
+    //   <h1 className="pt-4">Ingresos</h1>
+    //   <div className="charts-sales">
+    //     <div className="card">
+    //       <SalesChart />
+    //     </div>
+    //     <div className="card">
+    //       {/* <PieChartSales /> */}
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-1 border">1</div>
+        <div className="col-1 border">1</div>
+        <div className="col-1 border">1</div>
+        <div className="col-1 border">1</div>
+        <div className="col-1 border">1</div>
+        <div className="col-1 border">1</div>
+        <div className="col-1 border">1</div>
+        <div className="col-1 border">1</div>
+        <div className="col-1 border">1</div>
+        <div className="col-1 border">1</div>
+        <div className="col-1 border">1</div>
+        <div className="col-1 border">1</div>
+        <div className="col-1 border">1</div>
       </div>
     </div>
   );
