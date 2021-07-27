55ee6599f53685beada6a5da317f5b87358baa79
NNGR01 <guzman.r.nicolas@gmail.com>
2021-07-27 20:13:47 +0000

    registro

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -13,31 +13,31 @@ const SignUp = () => {
                     <form className="row g-3">
                         <div className="col-6">
                             <label for="inputEmail4" className="form-label">Nombre</label>
-                            <input type="text" className="form-control"/>
+                            <input type="text" className="form-control" id="primerNombre" name="primerNombre"/>
                         </div>
                         <div className="col-md-6">
                             <label for="inputPassword4" className="form-label">Segundo nombre</label>
-                            <input type="text" className="form-control"/>
+                            <input type="text" className="form-control" id="segundoNombre" name="segundoNombre"/>
                         </div>
                         <div className="col-6">
                             <label for="inputAddress" className="form-label">Apellido paterno</label>
-                            <input type="text" className="form-control"/>
+                            <input type="text" className="form-control" id="apellidoPaterno" name="apellidoPaterno"/>
                         </div>
                         <div className="col-6">
                             <label for="inputAddress2" className="form-label">Apellido materno</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" id="apellidoMaterno" name="apellidoMaterno" />
                         </div>
                         <div className="col-7"> 
                         <label for="inputAddress2" className="form-label">Correo</label>
-                            <input type="email" className="form-control" />
+                            <input type="text" className="form-control" placeholder="Ingresa Email" id="email" name="email"  />
                         </div>
                         <div className="col-md-5">
                             <label for="inputCity" className="form-label">Contraseña</label>
-                            <input type="password" className="form-control"/>
+                            <input className="form-control" type="password" id="contrasena" name="contrasena" placeholder="Contraseña" />
                         </div>
                         <div className="col-md-4">
                             <label for="inputZip" className="form-label">Teléfono</label>
-                            <input type="phone" className="form-control"/>
+                            <input type="phone" className="form-control" id="fono" name="fono"/>
                         </div>
                         <div className="col-12">
                             <button type="submit" className="btn btn-primary boton mt-5">Registrarse</button>
